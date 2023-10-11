ac922d8557e0df105aa378770b1f2ae0d02913a4
ajay sathvik <ajaysathvikk@gmail.com>
2023-10-12 01:14:55 +0530

    finished layout of the landing page

--- my-app/src/pages/LandingPage.js
+++ my-app/src/pages/LandingPage.js
@@ -2,6 +2,11 @@ import React from "react";
 import "./LandingPage.css";
 import image1 from "../assets/image1.jpeg";
 import fox from "../assets/Meta.png";
+import image2 from "../assets/image2.jpeg";
+import image3 from "../assets/image3.jpeg";
+import image4 from "../assets/image4.jpeg";
+import image5 from "../assets/image5.jpg";
+import image6 from "../assets/image6.jpg";
 
 const LandingPage = () => {
   return (
@@ -24,57 +29,64 @@ const LandingPage = () => {
           Researchers to conduct clinical trials that are more <br />
           Secure, Transparent, and Efficient.
         </div>
+      </div>
+      <div className="secret-heading">WHY ETHoMedix ??</div>
+      <div className="para1">
+        <div className="para1-title">Enhanced Data Security</div>
+        <div className="para1-subtitle">
+          EthoMedix is a Decentralized clinical trial platform <br /> that
+          allows Researchers to conduct clinical trials <br /> that are more
+          Secure, Transparent, and Efficient.
         </div>
-        <div className="secret-heading">WHY ETHoMedix ??</div>
-        <div className="para1">
-          <div className="para1-title">Enhanced Data Security</div>
-          <div className="para1-subtitle">
-            EthoMedix is a Decentralized clinical trial platform  <br /> that
-            allows Researchers to conduct clinical trials <br /> that are more
-            Secure, Transparent, and Efficient.
-          </div>
-          <img src={image1} className="image1" />
-        </div>
+        <img src={image1} className="image1" />
+      </div>
       <div className="para2">
         <div className="para2-title">Anonymous Participation</div>
         <div className="para2-subtitle">
-          EthoMedix's integration of DIDs empowers anonymous user involvement in
-          clinical trials, preserving personal information privacy.
+          EthoMedix's integration of DIDs empowers <br /> anonymous user
+          involvement in clinical trials, <br /> preserving personal information
+          privacy.
         </div>
+        <img src={image2} className="image2" />
       </div>
-      {/* <div className="para3">
+      <div className="para3">
         <div className="para3-title">Efficient Data Verification</div>
         <div className="para3-subtitle">
-          By securely storing decentralized identities on the blockchain,
-          EthoMedix balances anonymity with data verification, fostering trust
-          among stakeholders.
+          By securely storing decentralized identities on <br /> the blockchain,
+          EthoMedix balances anonymity <br /> with data verification, fostering
+          trust among <br /> stakeholders.
         </div>
-      </div> */}
-      {/* <div className="para4">
+        <img src={image3} className="image3" />
+      </div>
+      <div className="para4">
         <div className="para4-title">Technical Expertise</div>
         <div className="para4-subtitle">
-          EthoMedix thoroughly explores the technical intricacies of DIDs,
-          guaranteeing a seamless and secure experience for both patients and
-          researchers.
+          EthoMedix thoroughly explores the technical <br /> intricacies of
+          DIDs, guaranteeing a <br /> seamless and secure experience for both{" "}
+          <br /> patients and researchers.
         </div>
-      </div> */}
-      {/* <div className="para5">
-        <div className="para5-title">Streamlined Consent Managemen</div>
+        <img src={image4} className="image4" />
+      </div>
+      <div className="para5">
+        <div className="para5-title">Streamlined Consent Management</div>
         <div className="para5-subtitle">
-          EthoMedix simplifies consent procedures, ensuring a user-friendly
-          experience for patients and researchers, ultimately saving time and
-          effort.
+          EthoMedix simplifies consent procedures, ensuring <br /> a
+          user-friendly experience for patients & researchers <br /> ultimately
+          saving time & effort.
         </div>
-      </div> */}
-      {/* <div className="para6">
+        <img src={image5} className="image5" />
+      </div>
+      <div className="para6">
         <div className="para6-title">Real-World Impact</div>
         <div className="para6-subtitle">
-          EthoMedix demonstrates its potential with real-world case studies,
-          offering practical insights and highlighting its capacity to
-          revolutionize the pharmaceutical industry's approach to clinical
-          trials.
+          EthoMedix demonstrates its potential with real-world <br /> case
+          studies, offering practical insights & highlighting <br /> its
+          capacity to revolutionize the pharmaceutical <br /> industry's
+          approach to clinical trials.
         </div>
-      </div> */}
+        <img src={image6} className="image6" />
+      </div>
+      <div className="footer"></div>
     </div>
   );
 };
