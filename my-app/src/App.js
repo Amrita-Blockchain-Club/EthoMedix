f89d17bc612abd002941c46927b70fe099ee7bf1
ajay sathvik <ajaysathvikk@gmail.com>
2023-10-11 19:10:52 +0530

    navbar for the homepage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      EthoMedix
+      <LandingPage />
     </div>
   );
 }
---
+++ my-app/src/pages/LandingPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./LandingPage.css";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <div className="navbar">
+        <div className="navbar-container">
+          <div className="slogan">EthoMedix</div>
+          <div className="navbar-links">
+            <div className="about">About</div>
+            <div className="contact">Contact</div>
+            <div className="connect-wallet">Connect Wallet</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
