9b6a5f4c6a1c47667a98082edfd989f0a0ecd6cd
ajay sathvik <ajaysathvikk@gmail.com>
2023-10-19 13:37:47 +0530

    finished contact-us page and a needed things for the profile page user info

--- my-app/src/pages/Contact.js
+++ my-app/src/pages/Contact.js
@@ -1,10 +1,29 @@
 import React from "react";
 import Navbar from "./Navbar";
+import "./Contact.css";
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
+      <div className="contact-page">
+        <h1>Contact Us</h1>
+        <div className="contact-form">
+          <div className="contact-name">
+            <label htmlFor="name"> Name</label>
+            <input type="text" id="name" />
+          </div>
+          <div className="contact-email">
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" />
+          </div>
+          <div className="contact-message">
+            <label htmlFor="message">Message</label>
+            <textarea id="message" />
+          </div>
+          <button className="contact-submit">Submit</button>
+        </div>
+        </div>
     </div>
   );
 };
--- my-app/src/pages/Profile.js
+++ my-app/src/pages/Profile.js
@@ -16,6 +16,26 @@ const Profile = () => {
         <img src={user} className="user-image" />
         <div className="user-name-profile">Unknown</div>
         <div className="user-address-profile">{address}</div>
+        <div className="user-balance-profile">Balance: 0 ETH</div>
+        <div className="user-did-profile">Did Number</div>
+        <div className="user-order-history">
+          <h2>Order History</h2>
+            <div className="order-details">
+              <div className="order-name">Order Name</div>
+              <div className="order-price">Order Price</div>
+              <div className="order-status">Order Status</div>
+            </div>
+            <div className="order-details">
+              <div className="order-name">med1</div>
+              <div className="order-price">0.1 eth</div>
+              <div className="order-status">Pending</div>
+            </div>
+            <div className="order-details">
+              <div className="order-name">med2</div>
+              <div className="order-price">0.1 eth</div>
+              <div className="order-status">Pending</div>
+            </div>
+        </div>
       </div>
     </div>
   );
